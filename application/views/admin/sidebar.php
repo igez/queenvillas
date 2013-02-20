a0e7bac31a895b85fbf9644995fd65a73723a6a0
Robby Prima <robbiejobs@gmail.com>
2013-02-20 19:57:44 +0700

    Add landing page costumization link

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -7,6 +7,7 @@
 			<li><a class="ajax-link" href="/admin/message"><i class="icon-eye-open"></i><span class="hidden-tablet"> Messages</span></a></li>
 			<li><a class="ajax-link" href="/admin/gallery"><i class="icon-eye-open"></i><span class="hidden-tablet"> Gallery</span></a></li>
 			<li class="nav-header hidden-tablet">Contents</li>
+			<li><a class="ajax-link" href="/admin/posts?category=0"><i class="icon-align-justify"></i><span class="hidden-tablet"> Landing Page</span></a></li>
 			<li><a class="ajax-link" href="/admin/posts?category=1"><i class="icon-align-justify"></i><span class="hidden-tablet"> Accomodation</span></a></li>
 			<li><a class="ajax-link" href="/admin/posts?category=2"><i class="icon-calendar"></i><span class="hidden-tablet"> Facility</span></a></li>
 			<li><a class="ajax-link" href="/admin/posts?category=3"><i class="icon-th"></i><span class="hidden-tablet"> News</span></a></li>
