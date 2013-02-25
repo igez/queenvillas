20af20495487c4371dd123fa1e95046169cea313
Robby Prima <robbiejobs@gmail.com>
2013-02-25 15:28:45 +0700

    nav bar error fix on contact page

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -2,6 +2,15 @@
 
 class Contact extends CI_Controller {
 
+	var $data;
+	
+	public function __construct() {
+		parent::__construct();
+		$this->load->model(array('post_model', 'category_model', 'gallery_model'));
+		$this->data['nav'] = $this->post_model->fetchAll();
+		$this->load->vars($this->data);
+	}
+
 	public function index() {
 		$this->load->view('contact_page');
 	}
