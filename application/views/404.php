200e9796fe527a2df95b95c60c3ef7d8a3b476f8
Robby Prima <robbiejobs@gmail.com>
2013-03-03 12:27:48 +0700

    404 page now a bit beatiful, you dont say ?

---
+++ application/views/404.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Queen Villas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="/assets/css/style.css" />
+  <link rel="stylesheet" href="/assets/css/ui-lightness/jquery-ui-1.10.1.custom.css" />
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300' rel='stylesheet' type='text/css'>
+
+  <script type="text/javascript" src="/assets/js/jquery.js"></script>
+  <script type="text/javascript" src="/assets/js/modernizr.custom.79639.js"></script>
+    <script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/assets/js/jquery-ui-1.8.21.custom.min.js"></script>
+  <script type="text/javascript" src="/assets/js/jquery.ba-cond.min.js"></script>
+  <script type="text/javascript" src="/assets/js/jquery.slitslider.js"></script>
+
+  </head>
+  <style type="text/css">
+  .dotbg {
+    height: 300px;
+    position: relative;
+    z-index: -4;
+    background-image:url('/assets/img/atfot.gif') no-repeat; 
+    background-size:cover;
+  }
+  </style>
+  <body>
+    <?php $this->load->view('static/nav'); ?>
+      <div class="bg-head" style="background: gray;">
+    </div>
+    <style>
+      .bahe {
+        background: url('/assets/img/atfot.gif') repeat-x;
+        width: 100%;
+        height: 300px;
+      }
+    </style>
+    <div class="bahe">
+      <div class="title-container">
+        <div class="yellow-s"></div>
+        <div class="container">
+          <div class="yellow">
+            <h2>404 Not Found</h2>
+            <p><i>Queen Villas & Spa</i></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  
+    <div class="content container">
+      <div class="404" style="min-height: 300px;">
+        <h3>404 Not Found</h3>
+        <p>Whatever you looking for, it wasn't there anymore. Back to <a href="/">Home</a> ?</p>
+      </div>
+    </div>
+    <footer>
+      <div class="footer-logo pull-left">
+      </div>
+      <div class="footer-desc pull-left">
+        <p style="color: #695d58; font-weight: bold; font-size: 12px; padding-top: 10px;">Queen Villas & Spa</p>
+        <p>Gili Trawangan Island, North Lombok - Indonesia</p>
+      <p>ph: (62) 370-633686 (hunting) m: (62) 878-6450-4800 </p>
+      <p>fax: (62) 370-633626</p>
+      <p>email: info@queenvillas.com</p>
+      </div>
+      <div class="footer-soc">
+        <ul>
+          <li><img src="/assets/img/twitter.gif" alt="" /></li>
+          <li><img src="/assets/img/fb.gif" alt="" /></li>
+          <li><img src="/assets/img/mail.gif" alt="" /></li>
+        </ul>
+      </div>
+    </footer>
+  </body>
+</html>
