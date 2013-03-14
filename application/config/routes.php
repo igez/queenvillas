6d9877e30188238a3a2f3959ea456190ecbf829f
Robby Prima <robbiejobs@gmail.com>
2013-03-14 09:54:39 +0700

    New Route Added
    - Admin -> Slider_manager CRUD

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,7 +54,8 @@ $route['admin/posts'] = "admin/posts";
 $route['admin/post/save'] = "admin/post_save";
 $route['admin/post/update'] = "admin/post_update";
 $route['admin/setting'] = "admin/setting";
-$route['admin/setting/landing_page'] = "admin/setting_landing";
+$route['admin/setting/slideshow'] = "admin/setting_slideshow";
+$route['admin/setting/slideshow/delete'] = "admin/setting_slideshow_delete";
 $route['admin/message/isread/(:any)'] = "admin/msgIsRead";
 
 // RESERVATION
